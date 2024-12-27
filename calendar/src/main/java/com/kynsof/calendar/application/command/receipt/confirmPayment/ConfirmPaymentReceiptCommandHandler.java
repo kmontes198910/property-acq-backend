9e714b4b9adf7c984413dabbece372a160e0504c
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-12-27 17:10:40 -0500

    fix

--- calendar/src/main/java/com/kynsof/calendar/application/command/receipt/confirmPayment/ConfirmPaymentReceiptCommandHandler.java
+++ calendar/src/main/java/com/kynsof/calendar/application/command/receipt/confirmPayment/ConfirmPaymentReceiptCommandHandler.java
@@ -33,9 +33,9 @@ public class ConfirmPaymentReceiptCommandHandler implements ICommandHandler<Conf
             // Llamar al servicio externo para obtener el estado del pago
             paymentStatus = paymentServiceClient.callExternalService(command.getRequestId(), "279e33bd-471c-42ec-b0ac-ada8d82c6270");
 
-            if (paymentStatus == null || !paymentStatus.getStatus().equals("APPROVED")) {
-                throw new BusinessException(DomainErrorMessage.PAYMENT_NOT_FOUND, "Payment not approved or not found.");
-            }
+//            if (paymentStatus == null || !paymentStatus.getStatus().equals("APPROVED")) {
+//                throw new BusinessException(DomainErrorMessage.PAYMENT_NOT_FOUND, "Payment not approved or not found.");
+//            }
 
             // Actualizar el recibo con la información de pago
             ReceiptDto _receipt = this.service.findById(command.getReceiptId());
