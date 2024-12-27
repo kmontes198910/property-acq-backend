60664966e838c6f43b1205b4a8cf9d824a33cb22
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-12-26 23:54:25 -0500

    fix

--- share/src/main/java/com/kynsof/share/utils/ConfigureTimeZone.java
+++ share/src/main/java/com/kynsof/share/utils/ConfigureTimeZone.java
@@ -13,7 +13,6 @@ public class ConfigureTimeZone {
         
         return localDateTime;
     }
-
     public static LocalDateTime convertDateToLocalDateTime(Date date) {
         Instant instant = date.toInstant();
         ZonedDateTime zonedDateTime = instant.atZone(ZoneId.of(ZONE_ID));
