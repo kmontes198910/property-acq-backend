cedbd98f72626a5c250a64a1486729880957bf31
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2025-03-03 06:03:23 -0500

    Adicionando logica capaz de crear el paciente en caso de no existir en el micro evaluation.

--- evaluation/src/main/java/com/kynsof/evaluation/application/command/evaluation/create/CreateEvaluationCommandHandler.java
+++ evaluation/src/main/java/com/kynsof/evaluation/application/command/evaluation/create/CreateEvaluationCommandHandler.java
@@ -2,9 +2,13 @@ package com.kynsof.evaluation.application.command.evaluation.create;
 
 import com.kynsof.evaluation.domain.dto.EvaluationDto;
 import com.kynsof.evaluation.domain.dto.PatientDto;
+import com.kynsof.evaluation.domain.dto.enumDto.Status;
 import com.kynsof.share.core.domain.bus.command.ICommandHandler;
 import com.kynsof.evaluation.domain.service.IEvaluationService;
 import com.kynsof.evaluation.domain.service.IPatientsService;
+import com.kynsof.evaluation.infrastructure.service.http.PatientHttpUUIDService;
+import com.kynsof.share.core.domain.http.entity.PatientHttp;
+import java.time.LocalDate;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,16 +16,33 @@ public class CreateEvaluationCommandHandler implements ICommandHandler<CreateEva
 
     private final IEvaluationService serviceImpl;
     private final IPatientsService patientsService;
+    private final PatientHttpUUIDService patientHttpUUIDService;
 
     public CreateEvaluationCommandHandler(IEvaluationService serviceImpl,
-            IPatientsService patientsService) {
+            IPatientsService patientsService,
+            PatientHttpUUIDService patientHttpUUIDService) {
         this.serviceImpl = serviceImpl;
         this.patientsService = patientsService;
+        this.patientHttpUUIDService = patientHttpUUIDService;
     }
 
     @Override
     public void handle(CreateEvaluationCommand command) {
-        PatientDto patientDto = this.patientsService.findById(command.getPatient());
+        PatientDto patientDto = null;
+        try {
+            patientDto = this.patientsService.findById(command.getPatient());
+        } catch (Exception e) {
+            PatientHttp patient = patientHttpUUIDService.sendGetBookingHttpRequest(command.getPatient());
+            patientDto = new PatientDto(
+                    patient.getId(),
+                    patient.getIdentification(),
+                    patient.getName(),
+                    patient.getLastName(),
+                    Status.valueOf(patient.getStatus()),
+                    !patient.getBirthDate().equals("") ? LocalDate.parse(patient.getBirthDate()) : null
+            );
+            this.patientsService.create(patientDto);
+        }
         serviceImpl.create(new EvaluationDto(
                 command.getId(),
                 patientDto,
