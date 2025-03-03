77a93b77f343b9640582e58e4ff7377603ccc56e
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2025-03-03 06:01:31 -0500

    Adicionando entidad PatientHttp para replicar el paciente si no existe en los micros que lo necesiten

---
+++ share/src/main/java/com/kynsof/share/core/domain/http/entity/PatientHttp.java
@@ -0,0 +1,25 @@
+package com.kynsof.share.core.domain.http.entity;
+
+import com.kynsof.share.core.domain.bus.query.IResponse;
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class PatientHttp implements IResponse, Serializable {
+
+    private UUID id;
+    private String identification;
+    private String name;
+    private String lastName;
+    private String gender;
+    private String status;
+    private String birthDate;
+}
