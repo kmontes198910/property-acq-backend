cf3ce2b5d140a5eab0556d4a7d6762034bc04f13
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2025-03-03 06:02:15 -0500

    Adicionando servicio que devuelve PatientHttp.

---
+++ patients/src/main/java/com/kynsof/patients/application/query/patients/getByIdHttpReplicate/FindHttpPatientsByIdQueryHandler.java
@@ -0,0 +1,35 @@
+package com.kynsof.patients.application.query.patients.getByIdHttpReplicate;
+
+import com.kynsof.patients.domain.dto.PatientByIdDto;
+import com.kynsof.patients.domain.service.IPatientsService;
+import com.kynsof.share.core.domain.bus.query.IQueryHandler;
+import com.kynsof.share.core.domain.http.entity.PatientHttp;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FindHttpPatientsByIdQueryHandler implements IQueryHandler<FindHttpPatientsByIdQuery, PatientHttp>  {
+
+    private final IPatientsService serviceImpl;
+
+    public FindHttpPatientsByIdQueryHandler(IPatientsService serviceImpl) {
+        this.serviceImpl = serviceImpl;
+    }
+
+    @Override
+    public PatientHttp handle(FindHttpPatientsByIdQuery query) {
+        PatientByIdDto patient = serviceImpl.findById(query.getId());
+        String birthDate = "";
+        if (patient.getContactInfoDto() != null) {
+            birthDate = patient.getContactInfoDto().getBirthdayDate() != null ? patient.getContactInfoDto().getBirthdayDate().toString() : "";
+        }
+        return new PatientHttp(
+                patient.getId(), 
+                patient.getIdentification(), 
+                patient.getName(), 
+                patient.getLastName(), 
+                patient.getGender().name(), 
+                patient.getStatus().name(), 
+                birthDate
+        );
+    }
+}
