20a77c0cc23744720c4cbb090f260146b46d8168
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2025-03-12 22:25:05 -0500

    cambio de la cache del token

--- gateway/src/main/java/com/kynsoft/gateway/infrastructure/config/SecurityConfig.java
+++ gateway/src/main/java/com/kynsoft/gateway/infrastructure/config/SecurityConfig.java
@@ -1,37 +1,27 @@
 package com.kynsoft.gateway.infrastructure.config;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders;
+import org.springframework.security.oauth2.jwt.*;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableWebFluxSecurity
-@EnableReactiveMethodSecurity
 public class SecurityConfig {
 
-    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
-    private String jwkSetUri;
-	   
-    @Autowired
-    private JwtAuthConverter jwtAuthenticationConverter;
-    
+    private static final String JWK_SET_URI = "https://sso.kynsoft.net/realms/medinec/protocol/openid-connect/certs";
+
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
-                String[] AUTH_WHITELIST = {
-            // -- Swagger UI v2
-            "/api/**",};
         return httpSecurity
-        		.cors(Customizer.withDefaults())
+                .cors(cors -> cors.disable()) // Configurar CORS en application.properties
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchanges -> exchanges
         				.pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()
@@ -39,22 +29,29 @@ public class SecurityConfig {
                         .pathMatchers(HttpMethod.POST,"/payments-confirm/*").permitAll()
                         .pathMatchers(HttpMethod.GET, "/identity/api/auth/**").permitAll()
                         .pathMatchers(HttpMethod.GET, "/swagger-ui.html", "/swagger-ui/**", "/v2/api-docs.yaml", "/v3/api-docs.yaml", "/v2/api-docs/**", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**").permitAll()
-                        .pathMatchers(AUTH_WHITELIST).permitAll()
+                       // .pathMatchers(AUTH_WHITELIST).permitAll()
                         .anyExchange().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
-                                .jwt(jwtSpec -> jwtSpec
-                                                .jwtDecoder(jwtDecoder())
-                                                .jwtAuthenticationConverter(jwtAuthenticationConverter)
-                                )
+                        .jwt(jwt -> jwt
+                                .jwtDecoder(jwtDecoder()) // Usa validación local
+                        )
                 )
-                
                 .build();
     }
 
     @Bean
     public ReactiveJwtDecoder jwtDecoder() {
-    	return ReactiveJwtDecoders.fromIssuerLocation(jwkSetUri);
-    }
+        try {
+            NimbusReactiveJwtDecoder jwtDecoder = (NimbusReactiveJwtDecoder)
+                    ReactiveJwtDecoders.fromIssuerLocation("https://sso.kynsoft.net/realms/medinec");
 
+            return token -> Mono.fromSupplier(() -> jwtDecoder)
+                    .cache(Duration.ofMinutes(5))
+                    .flatMap(decoder -> decoder.decode(token));
+
+        } catch (Exception e) {
+            throw new RuntimeException("No se pudo inicializar el JWT Decoder", e);
+        }
+    }
 }
