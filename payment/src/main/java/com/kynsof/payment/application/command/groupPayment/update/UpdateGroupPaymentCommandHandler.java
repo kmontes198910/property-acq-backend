4dde9211db5ed30b1ffbb755798b5f778adbd9c9
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2025-03-27 12:57:49 -0500

    add ru,email and phone in calendar service

--- payment/src/main/java/com/kynsof/payment/application/command/groupPayment/update/UpdateGroupPaymentCommandHandler.java
+++ payment/src/main/java/com/kynsof/payment/application/command/groupPayment/update/UpdateGroupPaymentCommandHandler.java
@@ -1,29 +1,52 @@
 package com.kynsof.payment.application.command.groupPayment.update;
 
+import com.kynsof.payment.domain.dto.GroupPaymentDto;
+import com.kynsof.payment.domain.dto.enumDto.GroupPaymentStatus;
 import com.kynsof.payment.domain.service.IGroupPaymentService;
+import com.kynsof.payment.infrastructure.entity.GroupPayment;
+import com.kynsof.share.core.application.payment.domain.placeToPlay.PaymentServiceStatusResponse;
+import com.kynsof.share.core.application.payment.infrastructure.service.config.ExternalServiceClient;
 import com.kynsof.share.core.domain.bus.command.ICommandHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.List;
 
 @Component
 public class UpdateGroupPaymentCommandHandler implements ICommandHandler<UpdateGroupPaymentCommand> {
 
     private final IGroupPaymentService serviceImpl;
+    ;
+    private final ExternalServiceClient paymentServiceClient;
 
-    public UpdateGroupPaymentCommandHandler(IGroupPaymentService serviceImpl) {
+    public UpdateGroupPaymentCommandHandler(IGroupPaymentService serviceImpl, ExternalServiceClient paymentServiceClient) {
         this.serviceImpl = serviceImpl;
 
+        this.paymentServiceClient = paymentServiceClient;
     }
 
     @Override
     public void handle(UpdateGroupPaymentCommand command) {
-
+        GroupPaymentDto groupPaymentDto = serviceImpl.findById(command.getId());
         try {
-            this.serviceImpl.update(command.getId(), command.getReference(),
-                   command.getAuthorizationCode(),command.getRequestId(),
-                   command.getProcessUrl(),
-                   command.getStatus());
+            if (command.getStatus().equals(GroupPaymentStatus.PENDING_APPROVED) || command.getStatus().equals(GroupPaymentStatus.PAYMENT_APPROVED)) {
+                PaymentServiceStatusResponse paymentStatus = paymentServiceClient.validateStatusPayment(
+                        command.getRequestId(), groupPaymentDto.getBusiness().getId());
+                GroupPaymentStatus groupPaymentStatus = GroupPaymentStatus.PENDING_APPROVED;
+                if (paymentStatus.getStatus().equals("APPROVED")) {
+                    groupPaymentStatus = GroupPaymentStatus.PAYMENT_APPROVED;
+                } else if (paymentStatus.getStatus().equals("REJECTED")) {
+                    groupPaymentStatus = GroupPaymentStatus.REJECTED;
+                }
+                this.serviceImpl.update(groupPaymentDto.getId(), paymentStatus.getReference(), paymentStatus.getAuthorization(), groupPaymentDto.getRequestId(),
+                        command.getProcessUrl(), groupPaymentStatus
+                );
+            } else {
+                this.serviceImpl.update(command.getId(), command.getReference(),
+                        command.getAuthorizationCode(), command.getRequestId(),
+                        command.getProcessUrl(),
+                        command.getStatus());
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
