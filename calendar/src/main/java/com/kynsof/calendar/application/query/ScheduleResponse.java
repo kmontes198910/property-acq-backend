c01e0ee7893645a87babc953fed83df8ec552aea
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2025-03-28 17:50:44 -0500

    fix error

--- calendar/src/main/java/com/kynsof/calendar/application/query/ScheduleResponse.java
+++ calendar/src/main/java/com/kynsof/calendar/application/query/ScheduleResponse.java
@@ -28,6 +28,7 @@ public class ScheduleResponse implements IResponse {
     private LocalTime endingTime;
     private int stock;
     private EStatusSchedule status;
+    private boolean bloked;
 
     public ScheduleResponse(ScheduleDto schedule) {
         this.id = schedule.getId();
@@ -39,6 +40,7 @@ public class ScheduleResponse implements IResponse {
         this.stock = schedule.getStock();
         this.status = schedule.getStatus();
         this.service = new ServicesResponse(schedule.getService());
+        this.bloked = schedule.getStatus().equals(EStatusSchedule.BLOCKED);
     }
 
 }
