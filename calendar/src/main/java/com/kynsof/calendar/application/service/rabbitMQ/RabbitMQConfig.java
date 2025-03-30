bd80636ad7bcbbb19534ed9d0159ca5f84d21688
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2025-03-30 15:36:43 -0500

    fix error

--- calendar/src/main/java/com/kynsof/calendar/application/service/rabbitMQ/RabbitMQConfig.java
+++ calendar/src/main/java/com/kynsof/calendar/application/service/rabbitMQ/RabbitMQConfig.java
@@ -8,18 +8,11 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMQConfig {
 
     private static final String QUEUE_NAME = "paciente.calendar";
-    private static final String EXCHANGE_NAME = "paciente.exchange";
 
     @Bean
     public Queue queue() {
         return new Queue(QUEUE_NAME, true); // Cola durable
     }
-
-    @Bean
-    public FanoutExchange exchange() {
-        return new FanoutExchange(EXCHANGE_NAME);
-    }
-
     @Bean
     public Binding binding(Queue queue, FanoutExchange exchange) {
         return BindingBuilder.bind(queue).to(exchange);
