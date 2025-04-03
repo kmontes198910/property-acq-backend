46f758b208a9c732adf8af7b84b39f5f38ad201a
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2025-04-03 17:32:40 -0500

    fix error

--- treatments/src/main/java/com/kynsof/treatments/application/service/rabbitMQ/patientUpdate/RabbitMQPatientUpdateConfig.java
+++ treatments/src/main/java/com/kynsof/treatments/application/service/rabbitMQ/patientUpdate/RabbitMQPatientUpdateConfig.java
@@ -1,29 +1,30 @@
 package com.kynsof.treatments.application.service.rabbitMQ.patientUpdate;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.FanoutExchange;
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 @Configuration
 public class RabbitMQPatientUpdateConfig {
 
-    private static final String TREATMENT_QUEUE = "paciente.update.treatment";
-    private static final String EXCHANGE_NAME = "paciente.update.exchange";
+    private static final String UPDATE_QUEUE_NAME = "paciente.update.treatment";
+    private static final String UPDATE_EXCHANGE_NAME = "paciente.update.exchange";
 
-    @Bean
-    public Queue treatmentQueue() {
-        return new Queue(TREATMENT_QUEUE, true);
-    }
-    @Bean
-    public FanoutExchange pacienteExchange() {
-        return new FanoutExchange(EXCHANGE_NAME);
+    @Bean(name = "updateTreatmentQueue")
+    public Queue updateTreatmentQueue() {
+        return new Queue(UPDATE_QUEUE_NAME, true);
     }
 
+    @Bean(name = "updatePacienteExchange")
+    public FanoutExchange updatePacienteExchange() {
+        return new FanoutExchange(UPDATE_EXCHANGE_NAME);
+    }
+
     @Bean
-    public Binding bindTreatmentQueue(Queue treatmentQueue, FanoutExchange pacienteExchange) {
-        return BindingBuilder.bind(treatmentQueue).to(pacienteExchange);
+    public Binding bindUpdateTreatmentQueue(
+            @Qualifier("updateTreatmentQueue") Queue updateTreatmentQueue,
+            @Qualifier("updatePacienteExchange") FanoutExchange updatePacienteExchange) {
+        return BindingBuilder.bind(updateTreatmentQueue).to(updatePacienteExchange);
     }
 }
