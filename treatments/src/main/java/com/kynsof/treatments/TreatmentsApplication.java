5f81bbd7e5dfad1886d0a272acf1b8cc7dc833fa
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2025-04-09 16:04:16 -0500

    fix error

--- treatments/src/main/java/com/kynsof/treatments/TreatmentsApplication.java
+++ treatments/src/main/java/com/kynsof/treatments/TreatmentsApplication.java
@@ -1,5 +1,6 @@
 package com.kynsof.treatments;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -8,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableCaching
 @SpringBootApplication
 @EnableScheduling
+@EnableRabbit
 public class TreatmentsApplication {
 
     public static void main(String[] args) {
