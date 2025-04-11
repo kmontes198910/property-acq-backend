fe964561dcb27e46e60ecf497a30ad32358b9192
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2025-04-11 05:28:14 -0400

    Quitando las variables de entorno

--- identity/src/main/java/com/kynsof/identity/infrastructure/services/rabbitMq/RabbitMQBusinessConfig.java
+++ identity/src/main/java/com/kynsof/identity/infrastructure/services/rabbitMq/RabbitMQBusinessConfig.java
@@ -2,17 +2,14 @@ package com.kynsof.identity.infrastructure.services.rabbitMq;
 
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQBusinessConfig {
 
-    @Value("${rabbitmq.exchange.business}")
-    public String BUSINESS_EXCHANGE;
-    @Value("${rabbitmq.routing.key.business}")
-    public String BUSINESS_CREATED_ROUTING_KEY;
+    public static final String BUSINESS_EXCHANGE = "business.topic.exchange";
+    public static final String BUSINESS_CREATED_ROUTING_KEY = "business.routing.key";
 
     //Business
     @Bean
--- identity/src/main/java/com/kynsof/identity/infrastructure/services/rabbitMq/eventPublisher/EventPublisherService.java
+++ identity/src/main/java/com/kynsof/identity/infrastructure/services/rabbitMq/eventPublisher/EventPublisherService.java
@@ -1,11 +1,11 @@
 package com.kynsof.identity.infrastructure.services.rabbitMq.eventPublisher;
 
+import com.kynsof.identity.infrastructure.services.rabbitMq.RabbitMQBusinessConfig;
 import com.kynsof.identity.infrastructure.services.rabbitMq.dto.BusinessRabbitMQDto;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,9 +13,6 @@ public class EventPublisherService {
 
     private final RabbitTemplate rabbitTemplate;
     private final TopicExchange businessExchange;
-
-    @Value("${rabbitmq.routing.key.business}")
-    public String BUSINESS_CREATED_ROUTING_KEY;
 
     @Autowired
     public EventPublisherService(RabbitTemplate rabbitTemplate,
@@ -27,7 +24,7 @@ public class EventPublisherService {
     public void publishBusinessEvent(BusinessRabbitMQDto event) {
         rabbitTemplate.convertAndSend(
                 businessExchange.getName(),
-                BUSINESS_CREATED_ROUTING_KEY,
+                RabbitMQBusinessConfig.BUSINESS_CREATED_ROUTING_KEY,
                 event
         );
     }
