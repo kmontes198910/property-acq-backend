72e55451fc05906be5699637f3af991521ac9292
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2025-04-15 05:29:42 -0400

    Adicionando nuevos campos a la replica del Business

--- treatments/src/main/java/com/kynsof/treatments/application/service/rabbitMQ/consumer/impl/EventConsumerBusinessService.java
+++ treatments/src/main/java/com/kynsof/treatments/application/service/rabbitMQ/consumer/impl/EventConsumerBusinessService.java
@@ -1,20 +1,22 @@
 package com.kynsof.treatments.application.service.rabbitMQ.consumer.impl;
 
 import com.kynsof.treatments.application.service.rabbitMQ.Dto.BusinessRabbitMQDto;
+import com.kynsof.treatments.domain.dto.BusinessDto;
+import com.kynsof.treatments.domain.service.IBusiness;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EventConsumerBusinessService {
 
+    private final IBusiness business;
+
+    public EventConsumerBusinessService(IBusiness business) {
+        this.business = business;
+    }
+
     @RabbitListener(queues = "business.queue.treatments")
     public void handleCompanyEvent(BusinessRabbitMQDto event) {
-        System.err.println("#################################");
-        System.err.println("#################################");
-        System.err.println("Consume: " + event.getName());
-        System.err.println("Consume: " + event.getName());
-        System.err.println("Consume: " + event.getName());
-        System.err.println("#################################");
-        System.err.println("#################################");
+        this.business.create(new BusinessDto(event.getId(), event.getName(), event.getLogo()));
     }
 }
