b37467bd5f4830e75fda17a2c668d086607464df
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2025-04-17 07:22:17 -0500

    fix error

--- identity/src/main/java/com/kynsof/identity/domain/dto/ModuleDto.java
+++ identity/src/main/java/com/kynsof/identity/domain/dto/ModuleDto.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,7 +16,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @Getter
 @Setter
-public class ModuleDto {
+public class ModuleDto implements Serializable {
     protected UUID id;
     private String name;
     private String image;
