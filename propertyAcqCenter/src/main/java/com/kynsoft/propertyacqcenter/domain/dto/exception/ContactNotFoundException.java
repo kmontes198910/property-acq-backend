e4dc428bee781a41e1c3ff7627e2d3ddc588dd2a
tony8901 <tony.garces89@gmail.com>
2025-04-24 14:50:31 +0100

    fix: update exception message for contact not found

--- propertyAcqCenter/src/main/java/com/kynsoft/propertyacqcenter/domain/dto/exception/ContactNotFoundException.java
+++ propertyAcqCenter/src/main/java/com/kynsoft/propertyacqcenter/domain/dto/exception/ContactNotFoundException.java
@@ -2,6 +2,6 @@ package com.kynsoft.propertyacqcenter.domain.dto.exception;
 
 public class ContactNotFoundException extends RuntimeException {
     public ContactNotFoundException(String id, String property) {
-        super("Employee with " + property +": " + id + " not found");
+        super("Contact with " + property +": " + id + " not found");
     }
     }
