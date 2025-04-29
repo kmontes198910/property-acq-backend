11175b739058ba08f3738ff0fc1e25aa0f5b6121
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2025-04-29 05:57:48 -0400

    Agregando conexion con RabbitMQ para Business

---
+++ cirugia/src/main/java/com/kynsoft/cirugia/infrastructure/services/rabbitMQ/dto/BusinessRabbitMQDto.java
@@ -0,0 +1,30 @@
+package com.kynsoft.cirugia.infrastructure.services.rabbitMQ.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BusinessRabbitMQDto {
+    private UUID id;
+    private String name;
+    private String latitude;
+    private String longitude;
+    private String logo;
+    private String ruc;
+    private String address;
+    private String email;
+    private String phone;
+
+    @Override
+    public String toString() {
+        return "BusinessRabbitMQDto{" + "id=" + id + ", name=" + name + ", latitude=" + latitude + ", longitude=" + longitude + ", logo=" + logo + ", ruc=" + ruc + ", address=" + address + ", email=" + email + ", phone=" + phone + '}';
+    }
+
+}
