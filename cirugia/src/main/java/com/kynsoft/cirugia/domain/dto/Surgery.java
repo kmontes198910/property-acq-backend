c0aacb4a32789cca48bb5830b84fbcc7ea17f31a
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2025-05-05 10:53:13 -0500

    modulo de cirugia

--- cirugia/src/main/java/com/kynsoft/cirugia/domain/dto/Surgery.java
+++ cirugia/src/main/java/com/kynsoft/cirugia/domain/dto/Surgery.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -14,7 +15,7 @@ import java.util.UUID;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Surgery {
+public class Surgery implements Serializable {
     private UUID id;
     private UUID patientId;
     private UUID doctorId;
