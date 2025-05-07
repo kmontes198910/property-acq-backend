8fa452ec820464ae511c70f792a19bed4d11877e
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2025-05-07 16:20:45 -0500

    fix

--- cirugia/src/main/java/com/kynsoft/cirugia/application/query/recoveryroom/findbyid/RecoveryRoomResponse.java
+++ cirugia/src/main/java/com/kynsoft/cirugia/application/query/recoveryroom/findbyid/RecoveryRoomResponse.java
@@ -7,6 +7,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -14,7 +15,7 @@ import java.util.UUID;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class RecoveryRoomResponse implements IResponse {
+public class RecoveryRoomResponse implements IResponse, Serializable {
     private UUID id;
     private String name;
     private String description;
