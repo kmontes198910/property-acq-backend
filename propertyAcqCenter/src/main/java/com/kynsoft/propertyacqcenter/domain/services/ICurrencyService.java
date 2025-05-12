38beb6d69d56a4628edca828ce239b119032e32d
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2025-05-12 11:48:56 -0400

    Adicionando el search de Currency

---
+++ propertyAcqCenter/src/main/java/com/kynsoft/propertyacqcenter/domain/services/ICurrencyService.java
@@ -0,0 +1,21 @@
+package com.kynsoft.propertyacqcenter.domain.services;
+
+import com.kynsof.share.core.domain.request.FilterCriteria;
+import com.kynsof.share.core.domain.response.PaginatedResponse;
+import com.kynsoft.propertyacqcenter.domain.dto.CurrencyDto;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ICurrencyService {
+    UUID create(CurrencyDto object);
+    
+    void update(CurrencyDto object);
+    
+    void delete(UUID id);
+    
+    CurrencyDto findById(UUID id);
+    
+    PaginatedResponse search(Pageable pageable, List<FilterCriteria> filterCriteria);
+}
