f300386e147872b6431ff3ef5ffb5447206edd12
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2025-05-13 12:45:09 -0400

    Modificando las subCategorias de ContactType

--- propertyAcqCenter/src/main/java/com/kynsoft/propertyacqcenter/infrastructure/repository/command/SubCategoryWriteDataJPARepository.java
+++ propertyAcqCenter/src/main/java/com/kynsoft/propertyacqcenter/infrastructure/repository/command/SubCategoryWriteDataJPARepository.java
@@ -1,11 +1,11 @@
 package com.kynsoft.propertyacqcenter.infrastructure.repository.command;
 
-import com.kynsoft.propertyacqcenter.infrastructure.entity.SubCategoryConstructionType;
+import com.kynsoft.propertyacqcenter.infrastructure.entity.SubCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface SubCategoryConstructionTypeWriteDataJPARepository extends JpaRepository<SubCategoryConstructionType, UUID> {
+public interface SubCategoryWriteDataJPARepository extends JpaRepository<SubCategory, UUID> {
 }
