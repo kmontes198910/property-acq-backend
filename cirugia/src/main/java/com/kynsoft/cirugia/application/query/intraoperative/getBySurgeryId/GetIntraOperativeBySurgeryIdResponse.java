81b2971507b05295b9db45a5dfa127e06fd8ed9e
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2025-05-13 14:44:26 -0500

    fix

--- cirugia/src/main/java/com/kynsoft/cirugia/application/query/intraoperative/getBySurgeryId/GetIntraOperativeBySurgeryIdResponse.java
+++ cirugia/src/main/java/com/kynsoft/cirugia/application/query/intraoperative/getBySurgeryId/GetIntraOperativeBySurgeryIdResponse.java
@@ -4,6 +4,7 @@ import com.kynsof.share.core.domain.bus.query.IResponse;
 import com.kynsoft.cirugia.domain.dto.IntraOperative;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.time.LocalDate;
@@ -13,6 +14,7 @@ import java.util.UUID;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class GetIntraOperativeBySurgeryIdResponse implements IResponse, Serializable {
     private UUID id;
     private UUID surgeryId;
