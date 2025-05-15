64e5ce956495dda545780baa6241f6bf9f8ed893
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2025-05-15 16:14:21 -0500

    fix

--- cirugia/src/main/java/com/kynsoft/cirugia/application/command/bedassignment/create/CreateBedAssignmentCommandHandler.java
+++ cirugia/src/main/java/com/kynsoft/cirugia/application/command/bedassignment/create/CreateBedAssignmentCommandHandler.java
@@ -2,7 +2,9 @@ package com.kynsoft.cirugia.application.command.bedassignment.create;
 
 import com.kynsof.share.core.domain.bus.command.ICommandHandler;
 import com.kynsoft.cirugia.domain.dto.BedAssignment;
+import com.kynsoft.cirugia.domain.dto.RecoveryBed;
 import com.kynsoft.cirugia.domain.service.IBedAssignmentService;
+import com.kynsoft.cirugia.domain.service.IRecoveryBedService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -17,12 +19,21 @@ import java.util.UUID;
 public class CreateBedAssignmentCommandHandler implements ICommandHandler<CreateBedAssignmentCommand> {
 
     private final IBedAssignmentService bedAssignmentService;
+    private final IRecoveryBedService recoveryBedService;
 
     @Override
     @Transactional
     public void handle(CreateBedAssignmentCommand command) {
         log.info("Manejando comando para crear asignación de cama para cirugía: {}", command.getSurgeryId());
-        
+        RecoveryBed recoveryBed = recoveryBedService.findById(command.getBedId());
+        if (recoveryBed != null) {
+            // Actualizar el estado de la cama a "ASSIGNED"
+            recoveryBed.setStatus("ASSIGNED");
+            recoveryBedService.update(recoveryBed);
+        } else {
+            log.warn("Cama con ID {} no encontrada para actualizar su estado.", command.getBedId());
+        }
+
         // Crear DTO para la asignación de cama
         BedAssignment bedAssignment = BedAssignment.builder()
                 .id(command.getId() != null ? command.getId() : UUID.randomUUID())
@@ -44,7 +55,8 @@ public class CreateBedAssignmentCommandHandler implements ICommandHandler<Create
         
         // Llamar al servicio para crear la asignación (que también gestiona asignaciones previas)
         BedAssignment created = bedAssignmentService.createAndReplaceAssignment(bedAssignment);
-        
+
+
         // Configurar el mensaje de respuesta con la información de la asignación creada
         CreateBedAssignmentMessage responseMessage = CreateBedAssignmentMessage.builder()
                 .id(created.getId())
