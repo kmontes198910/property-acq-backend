86bed21dabc42c9399a325147d7ce363c0db25a5
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2025-05-20 14:28:16 -0500

    fix

--- invoice-service/src/main/java/com/kynsoft/invoiceservice/infrastructure/entities/Product.java
+++ invoice-service/src/main/java/com/kynsoft/invoiceservice/infrastructure/entities/Product.java
@@ -64,7 +64,7 @@ public class Product {
     @Column(name = "updated_by")
     private UUID updatedBy;
     
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "category_id")
     private ProductCategory category;
 }
