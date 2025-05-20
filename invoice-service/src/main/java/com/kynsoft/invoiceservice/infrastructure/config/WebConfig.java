15d9aa8dc3751ef4433ec7db72cef4960a393e3e
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2025-05-19 23:22:49 -0500

    fix

--- invoice-service/src/main/java/com/kynsoft/invoiceservice/infrastructure/config/WebConfig.java
+++ invoice-service/src/main/java/com/kynsoft/invoiceservice/infrastructure/config/WebConfig.java
@@ -5,12 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 /**
  * Configuración MVC para registrar nuestros conversores personalizados
+ * y configurar los recursos para Swagger UI
  */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -28,5 +30,13 @@ public class WebConfig implements WebMvcConfigurer {
         converters.removeIf(converter -> converter instanceof MappingJackson2HttpMessageConverter);
         MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter(objectMapper);
         converters.add(jsonConverter);
+    }
+    
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Configuración para Swagger UI
+        registry.addResourceHandler("/swagger-ui/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui/")
+                .resourceChain(false);
     }
 }
