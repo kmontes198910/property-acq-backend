b4753f5364a000e308e9b20d72d1bbac08007e61
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2025-05-24 13:32:46 -0500

    add crud medical test

--- medical-test/src/main/java/com/kynsoft/medicaltest/application/command/labTestRequest/create/CreateLabTestRequestRequest.java
+++ medical-test/src/main/java/com/kynsoft/medicaltest/application/command/labTestRequest/create/CreateLabTestRequestRequest.java
@@ -3,6 +3,8 @@ package com.kynsoft.medicaltest.application.command.labTestRequest.create;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
+
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,12 +15,21 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreateLabTestRequestRequest {
+    @NotNull(message = "El id es obligatorio")
     private UUID id;
+
+    @NotNull(message = "El paciente es obligatorio")
     private UUID patientId;
+
+    @NotNull(message = "El doctor es obligatorio")
     private UUID doctorId;
+
     private String status;
     private String observations;
+
+    @NotNull(message = "El businessId es obligatorio")
     private UUID businessId;
+
     private List<LabTestItemRequestRequest> labTestItems;
     private boolean isActive;
 }
