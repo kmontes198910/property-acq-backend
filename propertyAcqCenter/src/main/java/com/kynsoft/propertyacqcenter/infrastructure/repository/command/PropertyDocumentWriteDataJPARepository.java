5aad45cf4a97e6ab35435d39931e5d6dd12e65f1
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2025-05-24 05:45:12 -0400

    Adicionando la entidad: PropertyDocument hasta nivel de servicio

---
+++ propertyAcqCenter/src/main/java/com/kynsoft/propertyacqcenter/infrastructure/repository/command/PropertyDocumentWriteDataJPARepository.java
@@ -0,0 +1,11 @@
+package com.kynsoft.propertyacqcenter.infrastructure.repository.command;
+
+import com.kynsoft.propertyacqcenter.infrastructure.entity.PropertyDocument;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PropertyDocumentWriteDataJPARepository extends JpaRepository<PropertyDocument, UUID> {
+}
