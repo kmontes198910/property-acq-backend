899f7f1ef6521b7d16195e078bb83e7f613c165a
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2025-06-03 12:41:56 -0500

    fix error

--- treatments/src/main/java/com/kynsof/treatments/application/command/patientVaccine/delete/DeletePatientVaccineCommandHandler.java
+++ treatments/src/main/java/com/kynsof/treatments/application/command/patientVaccine/delete/DeletePatientVaccineCommandHandler.java
@@ -1,15 +1,15 @@
 package com.kynsof.treatments.application.command.patientVaccine.delete;
 
 import com.kynsof.share.core.domain.bus.command.ICommandHandler;
-import com.kynsof.treatments.domain.service.IPatientsService;
+import com.kynsof.treatments.domain.service.IPatientVaccineService;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DeletePatientVaccineCommandHandler implements ICommandHandler<PatientsPatientVaccineCommand> {
 
-    private final IPatientsService serviceImpl;
+    private final IPatientVaccineService serviceImpl;
 
-    public DeletePatientVaccineCommandHandler(IPatientsService serviceImpl) {
+    public DeletePatientVaccineCommandHandler(IPatientVaccineService serviceImpl) {
         this.serviceImpl = serviceImpl;
     }
 
