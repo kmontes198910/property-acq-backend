f093adcc05ee1300daf6d8d8cc83a6bdc2af9106
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2025-06-07 06:00:31 -0400

    Agregando nuevas variables al income

--- propertyAcqCenter/src/main/java/com/kynsoft/propertyacqcenter/application/command/income/create/CreateIncomeCommand.java
+++ propertyAcqCenter/src/main/java/com/kynsoft/propertyacqcenter/application/command/income/create/CreateIncomeCommand.java
@@ -19,7 +19,34 @@ public class CreateIncomeCommand implements ICommand {
     private Boolean increaseTypePercentage;
     private Boolean increaseFixedDollarAmount;
 
-    public CreateIncomeCommand(String property, Double grossMonthlyIncome, Double totalNetMonthlyIncome, Double increaseRate, Boolean increaseTypePercentage, Boolean increaseFixedDollarAmount) {
+    //Detail Breakdown
+    private Double unitType;
+    private Double quantity;
+    private Double rentMo;
+    private Double sqft;//Sqft
+    private Double sqftValue;//$/SqFt
+    private Double occupancy;
+    private Double annualIncrease;
+    //private Double avgMonthlyRent;//valor numérico auto calculable, se completa a partir del campo Rent/Mo.
+
+    //Micellaneous income
+    private Double depositForfeitures;
+    private Double sectino8Income;
+    private Double incomefromInterest;
+    private Double vendingMachines;
+    private Double lateCharges;
+    private Double laundryRoom;
+    private Double other;
+
+    //Management Fees
+    private Double propertyManagementRate;
+    private Double leasingCommissionRate;
+    private Double leasingCommision;
+
+    private Double porcentageIncreaseType;
+    private Double fixedDollarAmount;
+
+    public CreateIncomeCommand(String property, Double grossMonthlyIncome, Double totalNetMonthlyIncome, Double increaseRate, Boolean increaseTypePercentage, Boolean increaseFixedDollarAmount, Double unitType, Double quantity, Double rentMo, Double sqft, Double sqftValue, Double occupancy, Double annualIncrease, Double depositForfeitures, Double sectino8Income, Double incomefromInterest, Double vendingMachines, Double lateCharges, Double laundryRoom, Double other, Double propertyManagementRate, Double leasingCommissionRate, Double leasingCommision, Double porcentageIncreaseType, Double fixedDollarAmount) {
         this.id = UUID.randomUUID();
         this.property = property;
         this.grossMonthlyIncome = grossMonthlyIncome;
@@ -27,6 +54,25 @@ public class CreateIncomeCommand implements ICommand {
         this.increaseRate = increaseRate;
         this.increaseTypePercentage = increaseTypePercentage;
         this.increaseFixedDollarAmount = increaseFixedDollarAmount;
+        this.unitType = unitType;
+        this.quantity = quantity;
+        this.rentMo = rentMo;
+        this.sqft = sqft;
+        this.sqftValue = sqftValue;
+        this.occupancy = occupancy;
+        this.annualIncrease = annualIncrease;
+        this.depositForfeitures = depositForfeitures;
+        this.sectino8Income = sectino8Income;
+        this.incomefromInterest = incomefromInterest;
+        this.vendingMachines = vendingMachines;
+        this.lateCharges = lateCharges;
+        this.laundryRoom = laundryRoom;
+        this.other = other;
+        this.propertyManagementRate = propertyManagementRate;
+        this.leasingCommissionRate = leasingCommissionRate;
+        this.leasingCommision = leasingCommision;
+        this.porcentageIncreaseType = porcentageIncreaseType;
+        this.fixedDollarAmount = fixedDollarAmount;
     }
 
     public static CreateIncomeCommand fromRequest(CreateIncomeRequest request) {
@@ -36,7 +82,26 @@ public class CreateIncomeCommand implements ICommand {
                 request.getTotalNetMonthlyIncome(),
                 request.getIncreaseRate(),
                 request.getIncreaseTypePercentage(),
-                request.getIncreaseFixedDollarAmount()
+                request.getIncreaseFixedDollarAmount(),
+                request.getUnitType(),
+                request.getQuantity(),
+                request.getRentMo(),
+                request.getSqft(),
+                request.getSqftValue(),
+                request.getOccupancy(),
+                request.getAnnualIncrease(),
+                request.getDepositForfeitures(),
+                request.getSectino8Income(),
+                request.getIncomefromInterest(),
+                request.getVendingMachines(),
+                request.getLateCharges(),
+                request.getLaundryRoom(),
+                request.getOther(),
+                request.getPropertyManagementRate(),
+                request.getLeasingCommissionRate(),
+                request.getLeasingCommision(),
+                request.getPorcentageIncreaseType(),
+                request.getFixedDollarAmount()
         );
     }
 
--- propertyAcqCenter/src/main/java/com/kynsoft/propertyacqcenter/application/response/IncomeResponse.java
+++ propertyAcqCenter/src/main/java/com/kynsoft/propertyacqcenter/application/response/IncomeResponse.java
@@ -22,6 +22,33 @@ public class IncomeResponse implements Serializable, IResponse {
     private Boolean increaseTypePercentage;
     private Boolean increaseFixedDollarAmount;
 
+    //Detail Breakdown
+    private Double unitType;
+    private Double quantity;
+    private Double rentMo;
+    private Double sqft;//Sqft
+    private Double sqftValue;//$/SqFt
+    private Double occupancy;
+    private Double annualIncrease;
+    //private Double avgMonthlyRent;//valor numérico auto calculable, se completa a partir del campo Rent/Mo.
+
+    //Micellaneous income
+    private Double depositForfeitures;
+    private Double sectino8Income;
+    private Double incomefromInterest;
+    private Double vendingMachines;
+    private Double lateCharges;
+    private Double laundryRoom;
+    private Double other;
+
+    //Management Fees
+    private Double propertyManagementRate;
+    private Double leasingCommissionRate;
+    private Double leasingCommision;
+
+    private Double porcentageIncreaseType;
+    private Double fixedDollarAmount;
+
     public IncomeResponse(IncomeDto dto) {
         this.id = dto.getId();
         this.property = dto.getProperty() != null ? new PropertiesBasicResponse(dto.getProperty()) : null;
@@ -30,6 +57,29 @@ public class IncomeResponse implements Serializable, IResponse {
         this.increaseRate = dto.getIncreaseRate();
         this.increaseTypePercentage = dto.getIncreaseTypePercentage();
         this.increaseFixedDollarAmount = dto.getIncreaseFixedDollarAmount();
+
+        this.unitType = dto.getUnitType();
+        this.quantity = dto.getQuantity();
+        this.rentMo = dto.getRentMo();
+        this.sqft = dto.getSqft();
+        this.sqftValue = dto.getSqftValue();
+        this.occupancy = dto.getOccupancy();
+        this.annualIncrease = dto.getAnnualIncrease();
+
+        this.depositForfeitures = dto.getDepositForfeitures();
+        this.sectino8Income = dto.getSectino8Income();
+        this.incomefromInterest = dto.getIncomefromInterest();
+        this.vendingMachines = dto.getVendingMachines();
+        this.lateCharges = dto.getLateCharges();
+        this.laundryRoom = dto.getLaundryRoom();
+        this.other = dto.getOther();
+
+        this.propertyManagementRate = dto.getPropertyManagementRate();
+        this.leasingCommissionRate = dto.getLeasingCommissionRate();
+        this.leasingCommision = dto.getLeasingCommision();
+
+        this.porcentageIncreaseType = dto.getPorcentageIncreaseType();
+        this.fixedDollarAmount = dto.getFixedDollarAmount();
     }
 
 }
