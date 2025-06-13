e5a52443448da1e14074ae63c60c82a587a1e116
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2025-06-13 05:06:02 -0400

    Agregando sales al create AllAnalysis

---
+++ propertyAcqCenter/src/main/java/com/kynsoft/propertyacqcenter/application/command/allAnalysis/create/CreateAllAnalysisSalesRequest.java
@@ -0,0 +1,26 @@
+package com.kynsoft.propertyacqcenter.application.command.allAnalysis.create;
+
+import com.kynsoft.propertyacqcenter.domain.enums.PropertysAnnualValueIncrease;
+import com.kynsoft.propertyacqcenter.domain.enums.PropertysStarting;
+import com.kynsoft.propertyacqcenter.domain.enums.TypeOfSalesCost;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateAllAnalysisSalesRequest {
+
+    private Double capitalGainsTaxRate;
+    private Double stateIncomeTaxRate;
+    private Double federalIncomeTaxRate;
+    private Double purchesePrice;
+    private Double marketValueIndreaseRate;
+    private Boolean deprecationNone;
+    private Boolean deprecationStraightline;
+    private Boolean deprecationDoubleDecliningBalance;
+
+    private PropertysStarting propertysStarting;
+    private PropertysAnnualValueIncrease propertysAnnualValueIncrease;
+    private TypeOfSalesCost typeOfSalesCost;
+
+}
