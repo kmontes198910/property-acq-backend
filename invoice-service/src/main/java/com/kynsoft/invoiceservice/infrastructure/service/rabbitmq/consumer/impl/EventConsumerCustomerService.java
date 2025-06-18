e3be3cedc4c521a6e43deab18546d0edf588d570
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2025-06-18 12:56:24 -0500

    fix error

--- invoice-service/src/main/java/com/kynsoft/invoiceservice/infrastructure/service/rabbitmq/consumer/impl/EventConsumerCustomerService.java
+++ invoice-service/src/main/java/com/kynsoft/invoiceservice/infrastructure/service/rabbitmq/consumer/impl/EventConsumerCustomerService.java
@@ -6,6 +6,7 @@ import com.kynsoft.invoiceservice.domain.service.ICustomerService;
 import com.kynsoft.invoiceservice.infrastructure.entities.Customer;
 import com.kynsoft.invoiceservice.infrastructure.repository.query.CustomerReadRepository;
 import com.kynsoft.invoiceservice.infrastructure.service.rabbitmq.consumer.dto.RabbitMQPatientDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
@@ -13,27 +14,29 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Service
+@Slf4j
 public class EventConsumerCustomerService {
     private final ICustomerService service;
-    private final CustomerReadRepository query;
 
-    public EventConsumerCustomerService(ICustomerService service, CustomerReadRepository query) {
+    public EventConsumerCustomerService(ICustomerService service) {
         this.service = service;
-        this.query = query;
+
     }
     @RabbitListener(queues = "patient_customer.queue.invoice")
     public void handleCompanyEvent(RabbitMQPatientDto event) {
-        Optional<Customer> customer  = this.query.findByIdNumber(event.getIdentification());
-        if(customer.isPresent()){
-            CustomerDto customerDto = this.mapEntityToDto(customer.get());
-            customerDto.setEmail(event.getEmail());
-            customerDto.setAddress(event.getAddress());
-            customerDto.setPhoneNumber(event.getTelephone());
-            customerDto.setBusinessName(event.getName()+" "+event.getLastName());
-            this.service.update(customerDto);
-        }else{
+        log.error("Received event: {}", event);
+        try {
+            CustomerDto customer  = this.service.findByIdentificationNumber(event.getIdentification());
+                customer.setEmail(event.getEmail());
+                customer.setAddress(event.getAddress());
+                customer.setPhoneNumber(event.getTelephone());
+                customer.setBusinessName(event.getName()+" "+event.getLastName());
+                this.service.update(customer);
+        }catch (Exception e){
+            log.error("Error processing event: {}", e.getMessage());
+            log.error("Customer with identification {} not found, creating new customer", event.getIdentification());
             CustomerDto customerDto = CustomerDto.builder()
-                    .id(UUID.randomUUID())
+                    .id(event.getId() != null ? event.getId() : UUID.randomUUID())
                     .identificationType(event.getIdentificationType())
                     .identificationNumber(event.getIdentification())
                     .businessName(event.getName()+" "+event.getLastName())
@@ -44,25 +47,7 @@ public class EventConsumerCustomerService {
                     .createdBy(null)
                     .build();
             this.service.create(customerDto);
+            log.info("Customer created successfully: {}", customerDto);
         }
-
-
-
-    }
-    private CustomerDto mapEntityToDto(Customer customer) {
-        return CustomerDto.builder()
-                .id(customer.getId())
-                .identificationType(customer.getIdType())
-                .identificationNumber(customer.getIdNumber())
-                .businessName(customer.getBusinessName())
-                .address(customer.getAddress())
-                .email(customer.getEmail())
-                .phoneNumber(customer.getPhone())
-                .isActive(customer.getIsActive())
-                .createdAt(customer.getCreatedAt())
-                .updatedAt(customer.getUpdatedAt())
-                .createdBy(customer.getCreatedBy())
-                .updatedBy(customer.getUpdatedBy())
-                .build();
     }
 }
