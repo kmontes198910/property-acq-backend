e259f0700226d5261496edd6b4cff4629bc3ce27
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2025-06-20 17:39:02 -0500

    fix error

--- patients/src/main/java/com/kynsof/patients/PatientsApplication.java
+++ patients/src/main/java/com/kynsof/patients/PatientsApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cache.annotation.EnableCaching;
 public class PatientsApplication {
     public static void main(String[] args) {
         SpringApplication.run(PatientsApplication.class, args);
-
     }
 
 }
