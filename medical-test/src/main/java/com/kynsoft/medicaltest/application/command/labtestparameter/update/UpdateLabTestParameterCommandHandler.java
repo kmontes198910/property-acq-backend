b9a9b003d9c2d1791fe8c1d2f086e5ec373d6410
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2025-06-20 11:58:14 -0500

    fix error

--- medical-test/src/main/java/com/kynsoft/medicaltest/application/command/labtestparameter/update/UpdateLabTestParameterCommandHandler.java
+++ medical-test/src/main/java/com/kynsoft/medicaltest/application/command/labtestparameter/update/UpdateLabTestParameterCommandHandler.java
@@ -1,10 +1,14 @@
 package com.kynsoft.medicaltest.application.command.labtestparameter.update;
 
 import com.kynsof.share.core.domain.bus.command.ICommandHandler;
+import com.kynsoft.medicaltest.domain.dto.LabTestDto;
 import com.kynsoft.medicaltest.domain.dto.LabTestParameterDto;
 import com.kynsoft.medicaltest.domain.service.ILabTestParameterService;
+import com.kynsoft.medicaltest.domain.service.ILabTestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 /**
  * Manejador del comando para actualizar un parámetro de examen de laboratorio
@@ -14,12 +18,14 @@ import org.springframework.stereotype.Component;
 public class UpdateLabTestParameterCommandHandler implements ICommandHandler<UpdateLabTestParameterCommand> {
 
     private final ILabTestParameterService labTestParameterService;
+    private final ILabTestService    labTestService;
 
     @Override
     public void handle(UpdateLabTestParameterCommand command) {
         // Obtenemos el DTO actual para mantener los campos que no se actualizan
         LabTestParameterDto existingDto = labTestParameterService.getById(command.getId());
-
+        Optional<LabTestDto> labTestDto = labTestService.findById(command.getLabTestId());
+        labTestDto.ifPresent(existingDto::setLabTest);
         // Actualizamos solo los campos que pueden cambiar
         existingDto.setCode(command.getCode());
         existingDto.setName(command.getName());
