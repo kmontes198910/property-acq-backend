dba65aa21c6ee07024fe8790067a554cccd45650
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2025-07-04 16:27:54 -0400

    fix

--- identity/src/main/java/com/kynsof/identity/domain/interfaces/service/IUsersRolesService.java
+++ identity/src/main/java/com/kynsof/identity/domain/interfaces/service/IUsersRolesService.java
@@ -1,6 +1,7 @@
 package com.kynsof.identity.domain.interfaces.service;
 
 import com.kynsof.identity.controller.exception.ManageRole.UserSystemSetRolesDto;
+import com.kynsof.identity.domain.dto.ManageRolDto;
 import com.kynsof.identity.domain.dto.PermissionDto;
 import java.util.List;
 import java.util.UUID;
@@ -8,4 +9,5 @@ import java.util.UUID;
 public interface IUsersRolesService {
     void create(UserSystemSetRolesDto object);
     List<PermissionDto> permissions(UUID userId);
+    List<ManageRolDto> roles(UUID userId);
 }
