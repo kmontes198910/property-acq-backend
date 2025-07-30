faa148c1c2c404c182d2ec8c7da092db57e61ac8
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2025-07-30 15:39:27 -0400

    Agregando campos al search de companyContact

--- propertyAcqCenter/src/main/java/com/kynsoft/propertyacqcenter/application/response/CompanyContactSearchResponse.java
+++ propertyAcqCenter/src/main/java/com/kynsoft/propertyacqcenter/application/response/CompanyContactSearchResponse.java
@@ -2,7 +2,9 @@ package com.kynsoft.propertyacqcenter.application.response;
 
 import com.kynsof.share.core.domain.bus.query.IResponse;
 import com.kynsoft.propertyacqcenter.domain.dto.*;
+import com.kynsoft.propertyacqcenter.domain.enums.ContactType;
 import com.kynsoft.propertyacqcenter.domain.enums.DepartmentType;
+import com.kynsoft.propertyacqcenter.domain.enums.Type;
 import java.time.LocalDate;
 import lombok.*;
 
@@ -18,6 +20,10 @@ public class CompanyContactSearchResponse implements IResponse {
 
     private UUID id;
     private CompanyBasicResponse company;
+    private LegalEntityBasicResponse legalEntity;
+    private SubCategoryResponse subCategory;
+    private ContactType category;
+    private Type type;
     private LocalDate birthDate;
     private String firstName;
     private String lastName;
@@ -36,6 +42,10 @@ public class CompanyContactSearchResponse implements IResponse {
         this.id = dto.getId();
         this.birthDate = dto.getBirthDate();
         this.company = dto.getCompany() != null ? new CompanyBasicResponse(dto.getCompany()) : null;
+        this.legalEntity = dto.getLegalEntity() != null ? new LegalEntityBasicResponse(dto.getLegalEntity()) : null;
+        this.subCategory = dto.getSubCategory() != null ? new SubCategoryResponse(dto.getSubCategory()) : null;
+        this.category = dto.getCategory() != null ? dto.getCategory() : null;
+        this.type = dto.getType() != null ? dto.getType() : null;
         this.firstName = dto.getFirstName();
         this.lastName = dto.getLastName();
         this.email = dto.getEmail();
